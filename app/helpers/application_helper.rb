d227e913efcc31d9217f43868b223a58c87edacb
yuji hirai <yujihirai0420@gmail.com>
2019-08-10 17:01:46 +0900

    Add helper methods for page title and time format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def full_title(page_title = " ")
+    default_title = "Eventsboard -Create Events to Socialize"
+    if page_title.empty?
+      default_title
+    else
+      "#{page_title} | #{default_title}"
+    end
+  end
+
+  def time_format(event_date)
+    event_date.strftime("%Y-%m-%d %H:%M")
+  end
 end
