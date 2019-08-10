ebf2fc6ba5bfb1175a7a6bf302953b113c63e7a9
yuji hirai <yujihirai0420@gmail.com>
2019-08-10 12:16:26 +0900

    Add create and new actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,26 @@
 class EventsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      flash[:notice] = "Event created!"
+      redirect_to @event
+    else
+      flash.now[:alert] = "Event not created"
+      render 'new'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_date, :end_date, :vnue, :location)
+  end
 end
