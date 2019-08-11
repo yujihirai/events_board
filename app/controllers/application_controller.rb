a3944499529c7f638ca94edd6ec38eeb0ac916b1
yuji hirai <yujihirai0420@gmail.com>
2019-08-11 11:31:28 +0900

    Add strong parameter to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:email, :password, :password_confirmation, :first_name, :last_name, :username, :organization_name)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:email, :password, :password_confirmation, :first_name, :current_password, :last_name, :username,
+                  :organization_name)
+    end
+  end
 end
