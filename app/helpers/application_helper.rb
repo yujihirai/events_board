c084651277bd26ff6339a9e7d43cfaa0ff076009
yuji hirai <yujihirai0420@gmail.com>
2019-08-11 12:19:47 +0900

    Add user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
   def time_format(event_date)
     event_date.strftime("%Y-%m-%d %H:%M")
   end
+
+  def event_status(event)
+    if event.end_date <= Date.today
+      content_tag(:span, "", class: "past")
+    else
+      content_tag(:span, "", class: "upcoming")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :events
+  resources :users, only: [:show]
   root 'events#index'
 end
