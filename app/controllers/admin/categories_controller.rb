18a0470a619b186619103ce207530c8fc400834a
yuji hirai <yujihirai0420@gmail.com>
2019-08-19 12:53:46 +0900

    Ability to edit update and delete category by admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,10 @@
 class Admin::CategoriesController < Admin::ApplicationController
+  before_action :set_category, only: [:edit, :update, :destroy]
   def index
+    @categories = Category.order(:name)
+  end
 
+  def show
   end
 
   def new
@@ -11,7 +15,7 @@ class Admin::CategoriesController < Admin::ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      flash[:notice] = "Category has benn created."
+      flash[:notice] = "Category has been created."
       redirect_to admin_categories_path
     else
       flash.now[:alert] = "Category has not been created."
@@ -20,19 +24,30 @@ class Admin::CategoriesController < Admin::ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @category.update(category_params)
+      flash[:notice] = "Category has been updated."
+      redirect_to admin_categories_path
+    else
+      flash.now[:alert] = "Category has not updated."
+      render "edit"
+    end
   end
 
   def destroy
-
+    @category.destroy
+    flash[:danger] = "Category has been deleted."
+    redirect_to admin_categories_path
   end
 
   private
   def category_params
     params.require(:category).permit(:name, :summary)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
